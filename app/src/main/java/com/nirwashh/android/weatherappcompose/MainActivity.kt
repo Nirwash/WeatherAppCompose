c3c394e5538c08c9fbb20fc6e080f74b059343fc
Nirwash <nirwash@bk.ru>
2022-07-31 12:47:32 +0300

    добавление TabRow

--- app/src/main/java/com/nirwashh/android/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/nirwashh/android/weatherappcompose/MainActivity.kt
@@ -3,14 +3,14 @@ package com.nirwashh.android.weatherappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.nirwashh.android.weatherappcompose.screens.MainScreen
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.nirwashh.android.weatherappcompose.screens.MainCard
+import com.nirwashh.android.weatherappcompose.screens.TabLayout
 import com.nirwashh.android.weatherappcompose.ui.theme.WeatherAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +18,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppComposeTheme {
-                MainScreen()
+                Image(
+                    painter = painterResource(id = R.drawable.background),
+                    contentDescription = "background",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                    alpha = 0.95f
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
+
             }
         }
     }
--- app/src/main/java/com/nirwashh/android/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/nirwashh/android/weatherappcompose/screens/MainScreen.kt
@@ -3,11 +3,12 @@ package com.nirwashh.android.weatherappcompose.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -16,23 +17,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import com.nirwashh.android.weatherappcompose.R
 import com.nirwashh.android.weatherappcompose.ui.theme.MainColor
+import kotlinx.coroutines.launch
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Image(
-        painter = painterResource(id = R.drawable.background),
-        contentDescription = "background",
-        modifier = Modifier.fillMaxSize(),
-        contentScale = ContentScale.Crop,
-        alpha = 0.95f
-    )
+fun MainCard() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(4.dp, top = 44.dp)
+            .padding(start = 8.dp, top = 44.dp, end = 8.dp, bottom = 10.dp)
     ) {
         Card(
             modifier = Modifier.fillMaxWidth(),
@@ -57,15 +55,99 @@ fun MainScreen() {
                     AsyncImage(
                         model = "https://cdn.weatherapi.com/weather/64x64/day/113.png",
                         contentDescription = "Weather condition",
-                    modifier = Modifier.size(35.dp)
-                        .padding(top = 3.dp, end = 8.dp))
+                        modifier = Modifier
+                            .size(35.dp)
+                            .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "Moscow",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "31°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "search icon",
+                            tint = Color.White
+                        )
+                    }
+                    Text(
+                        text = "31°C/23°C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "sync icon",
+                            tint = Color.White
+                        )
+                    }
+
 
                 }
-
-
             }
         }
 
     }
 
+}
+
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("Hours", "Days")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .clip(shape = RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { position ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, position)
+                )
+            },
+            backgroundColor = MainColor,
+            contentColor = Color.White
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(selected = false, onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }, text = { Text(text = text) })
+            }
+
+        }
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) { index ->
+
+        }
+
+    }
 }
