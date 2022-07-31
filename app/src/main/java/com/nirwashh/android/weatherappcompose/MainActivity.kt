e9cf021ea8e2cfcee9388c3693cfac2d3d345da1
Nirwash <nirwash@bk.ru>
2022-07-31 15:05:20 +0300

    добавление LazyColumn

--- app/src/main/java/com/nirwashh/android/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/nirwashh/android/weatherappcompose/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.nirwashh.android.weatherappcompose.screens.ListItem
 import com.nirwashh.android.weatherappcompose.screens.MainCard
 import com.nirwashh.android.weatherappcompose.screens.TabLayout
 import com.nirwashh.android.weatherappcompose.ui.theme.WeatherAppComposeTheme
--- app/src/main/java/com/nirwashh/android/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/nirwashh/android/weatherappcompose/screens/MainScreen.kt
@@ -2,6 +2,7 @@ package com.nirwashh.android.weatherappcompose.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -146,6 +147,11 @@ fun TabLayout() {
             state = pagerState,
             modifier = Modifier.weight(1.0f)
         ) { index ->
+            LazyColumn( modifier = Modifier.fillMaxSize()) {
+                items(15) {
+                    ListItem()
+                }
+            }
 
         }
 
